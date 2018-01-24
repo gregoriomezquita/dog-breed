89a9b5c00a563e9b82d7bd4c8cc1c66470a6d93a
Gregorio Mezquita <gregoriomezquita@gmail.com>
2018-01-24 16:02:58 +0100

    app-engine-2 auth service

--- app-engine-2/main.py
+++ app-engine-2/main.py
@@ -14,23 +14,50 @@
 
 # [START app]
 import logging
-
 from flask import current_app, Flask, render_template, request, jsonify, send_from_directory
-#from pillow import Image, ImageOps
 from google.appengine.api import images
-from google.appengine.ext import ndb
+#from google.appengine.ext import ndb
 from google.appengine.api import app_identity
+from google.appengine.api import urlfetch
 import os
 import io
 import base64
 import json
-#import request
-
-token= app_identity.get_access_token('https://www.googleapis.com/auth/cloud-platform')[0]
+import numpy as np
+from google.appengine.api import urlfetch
 
 app = Flask(__name__)
 
-#url = 'https://ml.googleapis.com/v1/projects/{}/models/{}/versions/{}:predict&key={}'.format('udacity-190420','DogBreed','v1', auth)
+token= app_identity.get_access_token('https://www.googleapis.com/auth/cloud-platform')[0]
+
+with open('dog_names.json', 'r') as f: dog_names= json.load(f)
+
+def predict_rest(data):
+	try:
+		b64 = base64.urlsafe_b64encode(data)
+		input_instance = dict(inputs=b64)
+		input_instance = json.loads(json.dumps(input_instance))
+		request_body = {"instances": [input_instance]}
+ 		result = urlfetch.fetch(
+        url = 'https://ml.googleapis.com/v1/projects/{}/models/{}/versions/{}:predict'.format('udacity-190420','DogBreed','v1'),
+        payload=json.dumps(request_body),
+        method=urlfetch.POST,
+        headers={
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer %s" % token
+              }
+        )
+		response= json.loads(result.content)
+		if response.get('error') != None: print(response)
+		else:
+			predictions= response.get('predictions')[0].get('outputs')
+			max= np.argmax(predictions)
+			accuracy= predictions[max]* 100
+			return dog_names[max], accuracy
+		return '', 0.0
+	except urlfetch.Error: logging.exception('Caught exception fetching url')
+	
+	return '', 0.0
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -38,28 +65,26 @@ def home():
 		try:
 			img= request.files.get('image')
 			image_data= img.read()
-			image= images.Image(image_data=image_data)
-			#image_bytes= io.BytesIO(image_stream)
-			#img_b64= base64.b64encode(image_stream)
-			#img_b64_str= str(img_b64)
-			#image= images.Image(image_data=image_bytes)
+			
+			image= images.Image(image_data= image_data)
 			image.resize(width=224, height=224, crop_to_fit=True)
-			#image.im_feeling_lucky()
+			image.im_feeling_lucky()
 			thumbnail= image.execute_transforms(output_encoding=images.JPEG)
 
-			#img = request.files.get('image')
-			#image= io.BytesIO(img.read())
-			#image = ImageOps.fit(image, [224, 224], Image.ANTIALIAS)
 			img_b64_str= str(base64.b64encode(image_data))
-			#breed, acc= predict_raw(thumbnail)
-			img_b64_str= str(base64.b64encode(thumbnail))
-			breed, acc= None, 0.0
-			prediction= {'label':'HUMAN', 'breed':token, 'accuracy':acc}
+			#img_b64_str= str(base64.b64encode(thumbnail))
+			
+			breed, acc= predict_rest(thumbnail)
+			prediction= {'label':'DOG', 'breed':breed, 'accuracy':acc}
 		except Exception as e:
 			return jsonify(status_code='400', msg='Bad Request: %s' % str(e)), 400
 		return render_template('view.html', image_data= img_b64_str, file_name= img.filename, content_type= img.content_type, prediction= prediction)
 	else:
 		return render_template('form.html')
+
+@app.route('/_ah/auth', methods=['GET', 'POST'])
+def auth():
+	return app_identity.get_access_token('https://www.googleapis.com/auth/cloud-platform')[0]
 
 @app.route('/favicon.ico')
 def favicon():
